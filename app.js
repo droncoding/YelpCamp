a4946573542ee3c6ca8d4d56509812adf40b4358
droncoding <86107082+droncoding@users.noreply.github.com>
2021-07-01 16:20:20 +0800

    connect to db
    -set connection default
    -set db connection checkc
    -created campground Schema module

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", { 
+useUnifiedTopology: true,
+useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
+
+//cehck if db is connected
+const db = mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",()=>{
+    console.log("DB connected");
+});
+
 
 app.set("view engine","ejs");
 // app.set('views', path.join(__dirname,'views'))
