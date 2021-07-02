dc0c6f12b62862065fe0bf942e1d3ba694ed0eeb
droncoding <86107082+droncoding@users.noreply.github.com>
2021-07-02 15:32:42 +0800

    add campgrounds page
    -added sample files
    -generated random camp sites
    -generate campground title page

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.get("/",(req,res)=>{
     res.render("home")
 })
 
+app.get("/campgrounds",async(req,res)=>{
+    const campgrounds= await Campground.find({});
+    res.render("campgrounds/index",{campgrounds})
+})
+
 app.listen(3000,()=>{
     console.log("Server is listening on Port 3000");
 })
---
+++ seeds/index.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const {descriptors, places} =  require("./seedHelpers");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", { 
+useUnifiedTopology: true,
+useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
+
+//cehck if db is connected
+const db = mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",()=>{
+    console.log("DB connected");
+});
+
+const sample = array => array[Math.floor(Math.random()*array.length)];
+
+const seedDB = async()=>{
+    await Campground.deleteMany({});
+    for (i=0; i<50;i++){
+        const random1000 = Math.floor(Math.random()*1000);
+        const camp = new Campground({
+            location:`${cities[random1000].city},${cities[random1000].state}`,
+            title:`${sample(descriptors)} ${sample(places)}`});
+        await camp.save();
+    }
+    
+}
+
+seedDB();
