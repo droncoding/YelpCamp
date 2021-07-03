3a18eecfc97b70a924c51068d164a0892bcb770b
droncoding <86107082+droncoding@users.noreply.github.com>
2021-07-03 15:40:16 +0800

    Add "Add campground"

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+const bodyParser = require("body-parser");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", { 
 useUnifiedTopology: true,
@@ -19,6 +20,10 @@ db.once("open",()=>{
 app.set("view engine","ejs");
 // app.set('views', path.join(__dirname,'views'))
 
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
 
 app.get("/",(req,res)=>{
     res.render("home")
@@ -28,6 +33,24 @@ app.get("/campgrounds",async(req,res)=>{
     const campgrounds= await Campground.find({});
     res.render("campgrounds/index",{campgrounds})
 })
+
+app.get("/campgrounds/new",(req,res)=>{
+    res.render("campgrounds/new");
+})
+
+app.post("/campgrounds",async(req,res)=>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
+
+app.get("/campgrounds/:id", async(req,res, )=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show",{campground});
+
+});
 
 app.listen(3000,()=>{
     console.log("Server is listening on Port 3000");
