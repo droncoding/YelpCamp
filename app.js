3e7f0000afdb5d1ea3626db7d4b45aaf398524f4
droncoding <86107082+droncoding@users.noreply.github.com>
2021-07-04 22:27:11 +0800

    Add Edit&Delete functions

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const ejsMate = require("ejs-mate")
 const app = express();
 const mongoose = require("mongoose");
+const methodOverride = require('method-override');
 const Campground = require("./models/campground");
 const bodyParser = require("body-parser");
 
@@ -23,6 +25,7 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({
     extended:true
 }));
+app.use(methodOverride('_method'))
 
 
 app.get("/",(req,res)=>{
@@ -45,12 +48,27 @@ app.post("/campgrounds",async(req,res)=>{
 })
 
 
-
 app.get("/campgrounds/:id", async(req,res, )=>{
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show",{campground});
-
 });
+
+app.get("/campgrounds/:id/edit",async(req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit",{campground});
+});
+
+app.put('/campgrounds/:id',async(req,res)=>{
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id',async(req,res)=>{
+    const { id }= req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
 
 app.listen(3000,()=>{
     console.log("Server is listening on Port 3000");
