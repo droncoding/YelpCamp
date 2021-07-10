82dba6ebac91be25286fd1042195862421a41649
droncoding <86107082+droncoding@users.noreply.github.com>
2021-07-10 19:27:08 +0800

    Defining ExpressError class

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ejsMate = require("ejs-mate")
+const catchAsync = require("./utils/catchAsync");
 const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require('method-override');
@@ -32,42 +33,46 @@ app.get("/",(req,res)=>{
     res.render("home")
 })
 
-app.get("/campgrounds",async(req,res)=>{
+app.get("/campgrounds",catchAsync(async(req,res)=>{
     const campgrounds= await Campground.find({});
     res.render("campgrounds/index",{campgrounds})
-})
+}))
 
 app.get("/campgrounds/new",(req,res)=>{
     res.render("campgrounds/new");
 })
 
-app.post("/campgrounds",async(req,res)=>{
+app.post("/campgrounds",catchAsync(async(req,res,next)=>{
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 
-app.get("/campgrounds/:id", async(req,res, )=>{
+app.get("/campgrounds/:id", catchAsync(async(req,res, )=>{
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show",{campground});
-});
+}));
 
-app.get("/campgrounds/:id/edit",async(req,res)=>{
+app.get("/campgrounds/:id/edit",catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/edit",{campground});
-});
+}));
 
-app.put('/campgrounds/:id',async(req,res)=>{
+app.put('/campgrounds/:id',catchAsync(async(req,res)=>{
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.delete('/campgrounds/:id',async(req,res)=>{
+app.delete('/campgrounds/:id',catchAsync(async(req,res)=>{
     const { id }= req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.use((err,req,res,next)=>{
+    res.send("Something went wrong");
 })
 
 app.listen(3000,()=>{
