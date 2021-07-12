f804a53bcd4825c438a64ebb19ddb26d2ed13816
droncoding <86107082+droncoding@users.noreply.github.com>
2021-07-12 11:39:11 +0800

    Add Joi validation for new campground

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const methodOverride = require('method-override');
 const Campground = require("./models/campground");
 const bodyParser = require("body-parser");
+const Joi = require("joi");
+const { campgroundSchema } = require('./schemas');
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", { 
 useUnifiedTopology: true,
@@ -29,6 +31,17 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(methodOverride('_method'))
 
+const validateCampground = (req,res,next)=>{
+    
+    const { error } = campgroundSchema.validate(req.body);
+    if (error){
+        const msg = error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400)
+    }
+    else{
+        next();
+    }
+}
 
 app.get("/",(req,res)=>{
     res.render("home")
@@ -43,8 +56,9 @@ app.get("/campgrounds/new",(req,res)=>{
     res.render("campgrounds/new");
 })
 
-app.post("/campgrounds",catchAsync(async(req,res,next)=>{
-    if (!req.body.campground) throw new ExpressError('Invalid Campground info',400);
+app.post("/campgrounds",validateCampground,catchAsync(async(req,res,next)=>{
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground info',400);
+    
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -61,7 +75,7 @@ app.get("/campgrounds/:id/edit",catchAsync(async(req,res)=>{
     res.render("campgrounds/edit",{campground});
 }));
 
-app.put('/campgrounds/:id',catchAsync(async(req,res)=>{
+app.put('/campgrounds/:id',validateCampground,catchAsync(async(req,res)=>{
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`)
