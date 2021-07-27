63c09ed1f31723d378424c6151123cc7f3b4a11f
droncoding <86107082+droncoding@users.noreply.github.com>
2021-07-27 08:00:36 +0800

    Register form

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
-const campgrounds = require("./routes/campground");
-const reviews = require("./routes/reviews");
+const campgroundRoutes = require("./routes/campground");
+const reviewRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/user");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", { 
 useUnifiedTopology: true,
@@ -87,8 +88,9 @@ app.use((req,res,next)=>{
     next();
 })
 
-app.use("/campgrounds", campgrounds)
-app.use("/campgrounds/:id/reviews", reviews)
+app.use("/campgrounds", campgroundRoutes)
+app.use("/campgrounds/:id/reviews", reviewRoutes)
+app.use("/", userRoutes)
 
 app.use(passport.initialize());
 app.use(passport.session());
