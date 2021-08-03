e689398de51e7e9fb4ed3dcc64dd801f8506c8ff
droncoding <86107082+droncoding@users.noreply.github.com>
2021-08-03 12:26:12 +0800

    currentUser req.user

--- app.js
+++ app.js
@@ -85,6 +85,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req,res,next)=>{
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
