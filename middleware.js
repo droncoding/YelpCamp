74b91441d02e4019d8bb2646b13938a18bde1b02
droncoding <86107082+droncoding@users.noreply.github.com>
2021-08-07 17:46:05 +0800

    Authorization Middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const { campgroundSchema,reivewSchema } = require('./schemas');
+const ExpressError = require("./utils/ExpressError");
+const Campground = require("./models/campground");
+
+
 module.exports.isLoggedIn = (req,res,next)=>{
     if(! req.isAuthenticated()){
         req.session.returnTo = req.originalUrl;
@@ -5,4 +10,37 @@ module.exports.isLoggedIn = (req,res,next)=>{
         return res.redirect("/login");
     }
     next();
+}
+
+module.exports.validateCampground = (req,res,next)=>{
+    
+    const { error } = campgroundSchema.validate(req.body);
+    if (error){
+        const msg = error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400)
+    }
+    else{
+        next();
+    }
+}
+
+module.exports.isAuthor = async(req, res, next) =>{
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if(! campground.author.equals(req.user._id)){
+        req.flash("error","You dont have the permission!");
+        res.redirect(`/campgrounds/${campground._id}`)
+    }
+    next();
+}
+
+module.exports.validateReview = (req,res,next)=>{
+    const { error } = reivewSchema.validate(req.body);
+    if (error){
+        const msg = error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400)
+    }
+    else{
+        next();
+    }
 }
