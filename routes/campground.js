e83443f6c885324817eb3e5f2bd1a3ab39a1e3cb
droncoding <86107082+droncoding@users.noreply.github.com>
2021-08-08 18:38:27 +0800

    display&delete Reviews Authorization

--- routes/campground.js
+++ routes/campground.js
@@ -28,7 +28,11 @@ router.post("/",isLoggedIn,validateCampground,catchAsync(async(req,res,next)=>{
 
 
 router.get("/:id", catchAsync(async(req,res, )=>{
-    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    const campground = await Campground.findById(req.params.id).populate({path:'reviews',
+        populate:{
+        path:'author'
+        }
+    }).populate('author');
     console.log(campground);
     if(!campground){
         req.flash('error','Cant find the campground!');
--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router({mergeParams:true});
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-const { validateReview, isLoggedIn } =require("../middleware")
+const { validateReview, isLoggedIn, isReviewAuthor } =require("../middleware")
 
 const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
@@ -20,7 +20,7 @@ router.post('/', isLoggedIn, validateReview, catchAsync(async(req,res)=>{
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete('/:reviewId',catchAsync(async(req,res)=>{
+router.delete('/:reviewId',isLoggedIn, isReviewAuthor, catchAsync(async(req,res)=>{
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
