3f57135d3cf671fd31267660d711d295ad4c6f60
droncoding <86107082+droncoding@users.noreply.github.com>
2021-08-08 18:14:37 +0800

    Reviews Permissions

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,16 +2,17 @@ const express = require("express");
 const router = express.Router({mergeParams:true});
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-const { validateReview } =require("../middleware")
+const { validateReview, isLoggedIn } =require("../middleware")
 
 const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
 
 
 
-router.post('/',validateReview, catchAsync(async(req,res)=>{
+router.post('/', isLoggedIn, validateReview, catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
