edd5a1c5fa25516d8499ea7a8cd6313071819be5
droncoding <86107082+droncoding@users.noreply.github.com>
2021-08-16 10:22:33 +0800

    show file name when add pics

--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -1,6 +1,8 @@
 (function () {
     'use strict'
 
+    bsCustomFileInput.init()
+
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.validated-form')
 
