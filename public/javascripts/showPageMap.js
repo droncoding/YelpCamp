46d3c8180e8d954ac5b2af26d9db0f9da8709f39
droncoding <86107082+droncoding@users.noreply.github.com>
2021-08-23 20:44:21 +0800

    Center The Map On A Campground

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,11 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 4 // starting zoom
+    center: campground.geometry.coordinates, 
+    //center: [-74.5, 40],
+    zoom: 8 
 });
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map)
