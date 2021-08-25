b170b5dfcb06ee0674d6a7b5b19c966fb80f94e2
droncoding <86107082+droncoding@users.noreply.github.com>
2021-08-25 10:37:21 +0800

    Styling Home Page

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,7 +3,6 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: campground.geometry.coordinates, 
-    //center: [-74.5, 40],
     zoom: 8 
 });
 
