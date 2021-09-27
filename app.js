9913eab085a4e9f9c988b9679c705ba5d0eaeb35
droncoding <86107082+droncoding@users.noreply.github.com>
2021-09-27 19:24:42 -0400

    ready to deploy

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const MongoDBStore = require("connect-mongo");
 
 //const dbUrl = process.env.DB_URL;
 //"mongodb://localhost:27017/yelp-camp"
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL ||"mongodb://localhost:27017/yelp-camp";
 mongoose.connect(dbUrl, { 
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -82,18 +82,20 @@ const validateReview = (req,res,next)=>{
     }
 }
 
+const secret = process.env.SECRET ||'firstappisdevelopibf';
+
 const store = new MongoDBStore({
     mongoUrl:dbUrl,
-    secret: 'firstappisdevelopibf',
+    secret,
     touchAfter: 24 * 60 *60
 });
 
-// store.on("error",function(e){
-//     console.log("SESSION STORE ERROR",e)
-// })
+store.on("error",function(e){
+    console.log("SESSION STORE ERROR",e)
+})
 
 const sessionConfig = {
-    //store,
+    store,
     name: 'session',
     secret:'Justasmallsecret',
     resave:false,
