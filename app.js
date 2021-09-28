4bccad0522f1402cc380a63cda3aea3edef82228
droncoding <86107082+droncoding@users.noreply.github.com>
2021-09-28 11:24:15 -0400

    add start script

--- app.js
+++ app.js
@@ -198,6 +198,8 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{err})
 })
 
-app.listen(3000,()=>{
-    console.log("Server is listening on Port 3000");
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Server is listening on Port ${port}` );
 })
